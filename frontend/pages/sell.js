bb35d65d9efd38543be4ecd8499d39d852163c6e
Karla Dampilag <karla.dmplg@gmail.com>
2020-03-08 00:47:00 +0800

    gated sign in component

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignIn';
 
 const Sell = (props) => {
     return (
-        <>
+        <PleaseSignIn>
             <CreateItem />
-        </>
+        </PleaseSignIn>
     )
 }
 export default Sell;
